2b399d6584425e2b9f9b87024ce08a71ee8d42a8
Champulin <Champustrike@gmail.com>
2022-07-13 18:08:50 +0200

    urls and reqs

--- pgmproject/urls.py
+++ pgmproject/urls.py
@@ -1,4 +1,4 @@
-"""pgmproject URL Configuration
+"""pgm_projectdir URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -10,12 +10,21 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include, path1
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('pgm_app.urls')),
+    path('', RedirectView.as_view(url='', permanent=True)),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=settings.STATIC_URL + "/favicon.ico"),
+    ),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
