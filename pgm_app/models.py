b9d9b7009462eca9a9a2bf7c84fdfbe2887b494f
Champulin <Champustrike@gmail.com>
2022-07-16 13:41:24 +0200

    models.py correct

--- pgm_app/models.py
+++ pgm_app/models.py
@@ -5,12 +5,12 @@ class Project(models.Model):
     title = models.CharField(max_length=100, help_text="Titulo del Proyecto", verbose_name="Titulo")
     subtitle = models.CharField(max_length=100, help_text="Subtitulo", verbose_name="Subtitulo")
     image = models.ImageField(upload_to='media/images',blank=True, help_text="Imagen del Proyecto", verbose_name="Imagen")
-    slug = models.SlugField(max_length=100, help_text="Slug del Proyecto", verbose_name="Slug")
+    slug = models.SlugField(max_length=100, help_text="No tocar", verbose_name="Slug")
     description = models.TextField(help_text="Descripcion del Proyecto", verbose_name="Descripcion")
     date = models.DateField()
     video_url = models.CharField(max_length=200, blank=True,help_text='Url del video ', verbose_name='Video URL')
-    source = models.ForeignKey('Source', on_delete=models.CASCADE,blank=True, help_text='Youtube ou Vimeo',verbose_name='Source')
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, help_text='choisi la catégorie du video', verbose_name='Categoria')
+    source = models.ForeignKey('Source', on_delete=models.CASCADE,blank=True, help_text='Youtube o Vimeo o NONE si no hay video',verbose_name='Source')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, help_text='Categoria del projecto', verbose_name='Categoria')
     # Metadata
     class Meta:
         ordering = ['-date','title']
