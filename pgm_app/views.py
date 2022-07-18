373d7ccf8fdd1be39b21c8a9d95999edccfb684a
Champulin <Champustrike@gmail.com>
2022-07-18 19:06:43 +0200

    changes to view

--- pgm_app/views.py
+++ pgm_app/views.py
@@ -5,10 +5,10 @@ class IndexListView(generic.ListView):
     template_name = 'index.html'
     context_object_name = 'projects_list'
     def get_queryset(self):
-        return Project.objects.all().order_by('-date')
+        return Project.objects.all().order_by('date')
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['category_list'] = Category.objects.all().order_by('-id')
+        context['category_list'] = Category.objects.all().order_by('id')
         return context
 
 class ProjectDetailView(generic.DetailView):
